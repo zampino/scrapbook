ab36c812034ebd986fa753b660453315bfdfceb9
Andrea Amantini <lo.zampino@gmail.com>
2024-01-24 15:24:22 +0100

    Fit for latest Garden

--- ops/ops.clj
+++ ops/ops.clj
@@ -3,10 +3,7 @@
             [nextjournal.clerk :as clerk]
             [nextjournal.clerk.viewer :as viewer]
             [clojure.java.shell :refer [sh]]
-            [nextjournal.clerk.webserver :as webserver]
-            [nrepl.server]))
-
-(defonce !nrepl (atom nil))
+            [nextjournal.clerk.webserver :as webserver]))
 
 (def sha (-> (sh "git" "rev-parse" "--short" "HEAD" :out :string) :out str/trim))
 
@@ -33,27 +30,8 @@
 (reset! webserver/!doc {:nav-path "index"})
 
 (defn boot [opts]
-  (reset! !nrepl (nrepl.server/start-server :bind "0.0.0.0" :port 6666))
-
-  ;; cannot create concurrent connections, e.g. caused by old container during re-deploy of a new version
-  ;;
-  ;; Execution error (ExceptionInfo) at nextjournal.clerk.eval/eval+cache! (eval.clj:155).
-  ;; Execution error (LmdbNativeException$ConstantDerivedException) at org.lmdbjava.ResultCodeMapper/checkRc (ResultCodeMapper.java:114).
-  ;; Platform constant error code: EAGAIN Resource temporarily unavailable (11)
-  ;;
-  ;; see Caveats section at http://www.lmdb.tech/doc/
-
-  #_
-  (clerk/show! "notebooks/todo.clj")
-
-  ;; hack to get have / go to notebook
-
-
-  ;; disable header
-
-  ;; boot clerk
+  (println "Clerk with custom headers.")
   (clerk/serve! opts))
 
 (defn stop []
-  (nrepl.server/stop-server @!nrepl)
   (clerk/halt!))
